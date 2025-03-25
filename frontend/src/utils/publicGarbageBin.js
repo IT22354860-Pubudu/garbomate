6cb6cee0f616f6d59ee38423109b193cd22e3526
II22354860-Pubudu <it22354860@my.sliit.lk>
2025-03-25 22:00:38 +0530

    Add publicGarbageLocations utility for managing public garbage bin locations and types

---
+++ frontend/src/utils/publicGarbageBin.js
@@ -0,0 +1,36 @@
+const publicGarbageLocations = [
+    { town: 'Samanala Uyana', longitude: 79.972276, latitude: 6.913966, type: ['Recyclable', 'Hazardous', 'Organic', 'E-Waste', 'Non-Recyclable'] },
+    { town: 'Welivita Rd', longitude: 79.972258, latitude: 6.915784, type: ['Non-Recyclable', 'Organic', 'Recyclable'] },
+    { town: 'Gamunupura', longitude: 79.973697, latitude: 6.917240, type: ['Hazardous', 'Non-Recyclable', 'E-Waste', 'Organic', 'Recyclable'] },
+    { town: 'Hokandara', longitude: 79.977859, latitude: 6.890611, type: ['Non-Recyclable', 'Organic', 'Hazardous', 'Recyclable'] },
+    { town: 'Nawagamuwa', longitude: 80.019739, latitude: 6.923010, type: ['Recyclable', 'E-Waste', 'Organic', 'Non-Recyclable'] },
+    { town: 'Bathtaramulla', longitude: 79.917535, latitude: 6.901681, type: ['Hazardous', 'Organic', 'Recyclable', 'Non-Recyclable'] },
+    { town: 'Hanwalla', longitude: 80.088593, latitude: 6.901649, type: ['Recyclable', 'Hazardous', 'Organic', 'E-Waste', 'Non-Recyclable'] },
+    { town: 'Athurugiriya', longitude: 79.981998, latitude: 6.904600, type: ['Recyclable', 'Organic', 'Non-Recyclable'] },
+    { town: 'Pannipitiya', longitude: 79.958267, latitude: 6.842230, type: ['Organic', 'Hazardous', 'Non-Recyclable', 'E-Waste', 'Recyclable'] },
+    { town: 'Thalawathugoda', longitude: 79.952376, latitude: 6.858500, type: ['Recyclable', 'Organic', 'E-Waste', 'Non-Recyclable'] },
+    { town: 'Malabe', longitude: 79.957699, latitude: 6.902700, type: ['Non-Recyclable', 'Hazardous', 'Organic', 'Recyclable'] },
+    { town: 'Kaduwela', longitude: 79.990698, latitude: 6.936156, type: ['E-Waste', 'Non-Recyclable', 'Hazardous', 'Organic', 'Recyclable'] },
+    { town: 'Rajagiriya', longitude: 79.901793, latitude: 6.918536, type: ['Organic', 'Hazardous', 'Non-Recyclable', 'Recyclable'] },
+    { town: 'Maharagama', longitude: 79.957347, latitude: 6.845771, type: ['E-Waste', 'Organic', 'Recyclable', 'Non-Recyclable'] },
+    { town: 'Dehiwala', longitude: 79.881670, latitude: 6.837243, type: ['Non-Recyclable', 'E-Waste', 'Organic', 'Hazardous', 'Recyclable'] },
+    { town: 'Nugegoda', longitude: 79.899878, latitude: 6.864926, type: ['Recyclable', 'Hazardous', 'Organic', 'Non-Recyclable'] },
+    { town: 'Koswatta', longitude: 79.914719, latitude: 6.923171, type: ['E-Waste', 'Non-Recyclable', 'Recyclable', 'Organic'] },
+    { town: 'Kalapaluwawa', longitude: 79.917587, latitude: 6.944561, type: ['Non-Recyclable', 'E-Waste', 'Organic', 'Recyclable'] },
+    { town: 'Kelaniya', longitude: 79.926247, latitude: 6.958542, type: ['Recyclable', 'Organic', 'Non-Recyclable', 'Hazardous'] },
+    { town: 'Peliyagoda', longitude: 79.921714, latitude: 6.955541, type: ['Hazardous', 'Organic', 'Recyclable', 'Non-Recyclable'] },
+    { town: 'Kiribathgoda', longitude: 79.928848, latitude: 6.978305, type: ['E-Waste', 'Recyclable', 'Organic', 'Non-Recyclable'] },
+    { town: 'Kadawatha', longitude: 79.970688, latitude: 6.976125, type: ['Recyclable', 'E-Waste', 'Non-Recyclable', 'Organic'] },
+    { town: 'Ragama', longitude: 79.944838, latitude: 7.037604, type: ['Hazardous', 'Organic', 'E-Waste', 'Non-Recyclable', 'Recyclable'] },
+    { town: 'Wattala', longitude: 79.881165, latitude: 6.991211, type: ['E-Waste', 'Organic', 'Recyclable', 'Non-Recyclable'] },
+    { town: 'Ja-Ela', longitude: 79.894775, latitude: 7.059438, type: ['Recyclable', 'Organic', 'Hazardous', 'Non-Recyclable'] },
+    { town: 'Ekala', longitude: 79.917031, latitude: 7.065491, type: ['Hazardous', 'Non-Recyclable', 'E-Waste', 'Organic', 'Recyclable'] },
+    { town: 'Minuwangoda', longitude: 79.949535, latitude: 7.189362, type: ['Organic', 'E-Waste', 'Recyclable', 'Non-Recyclable'] },
+    { town: 'Katunayake', longitude: 79.888244, latitude: 7.171048, type: ['Organic', 'Recyclable', 'Hazardous', 'E-Waste', 'Non-Recyclable'] },
+    { town: 'Seeduwa', longitude: 79.913443, latitude: 7.144146, type: ['Organic', 'Non-Recyclable', 'Recyclable'] },
+    { town: 'Negombo', longitude: 79.857725, latitude: 7.209587, type: ['E-Waste', 'Organic', 'Hazardous', 'Recyclable', 'Non-Recyclable'] }
+];
+
+
+
+  export { publicGarbageLocations };
